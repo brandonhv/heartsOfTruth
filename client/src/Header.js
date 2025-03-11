add32b319fdb354a03b1df105d31f1acffe1e4e6
Brandon <brandon5.9@hotmail.com>
2025-03-11 19:35:10 -0400

    React headroom has been added

---
+++ client/src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Headroom from 'react-headroom';
+
+
+const Header = () => {
+
+    return (
+        <Headroom className="App-header">
+        <h1>You can put anything you'd like inside the Headroom Component</h1>
+        </Headroom>
+    );
+};
+
+export default Header;
