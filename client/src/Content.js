cc8303cd4375b9b0be46657d651884d0c7e09e69
Brandon <brandon5.9@hotmail.com>
2025-03-10 23:32:51 -0400

    Organizing content

---
+++ client/src/Content.js
@@ -0,0 +1,13 @@
+import React from "react";
+import mainImage from "./IMG_8763.JPG";
+
+
+const Content = () => {
+
+    return (
+        <img src={mainImage} className="App-logo" alt="helkjalisdjf" />
+    ) 
+};
+
+
+export default Content;
