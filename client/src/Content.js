b24ddd229c334ad44eec19b00c099d4a43f646cd
Brandon <brandon5.9@hotmail.com>
2025-03-15 00:17:26 -0400

    images

--- client/src/Content.js
+++ client/src/Content.js
@@ -2,7 +2,7 @@ import React from "react";
 import mainImage from "./photos/IMG_8763.JPG";
 import community from "./photos/community.png";
 import construction from "./photos/construction.png"
-import mission from "./photos/mission.png"
+// import mission from "./photos/mission.png"
 
 
 const Content = () => {
@@ -13,7 +13,7 @@ const Content = () => {
             <img src={mainImage} className="App-logo" alt="" />
             <img src={community} className="community" alt="" />
             <img src={construction} className="construction" alt="" />
-            <img src={mission} className="mission" alt="" />
+            {/* <img src={mission} className="mission" alt="" /> */}
         </div>
         
     ) 
