6256e16065ba1d130843bd0b96f45a8c6399a19c
Brandon <brandon5.9@hotmail.com>
2025-03-15 00:21:36 -0400

    reminder image has been added

--- client/src/Content.js
+++ client/src/Content.js
@@ -1,8 +1,8 @@
 import React from "react";
 import mainImage from "./photos/IMG_8763.JPG";
 import community from "./photos/community.png";
-// import construction from "./photos/construction.png"
-// import mission from "./photos/mission.png"
+import construction from "./photos/construction.png"
+import mission from "./photos/mission.png"
 
 
 const Content = () => {
@@ -12,8 +12,8 @@ const Content = () => {
         <div>
             <img src={mainImage} className="App-logo" alt="" />
             <img src={community} className="community" alt="" />
-            {/* <img src={construction} className="construction" alt="" /> */}
-            {/* <img src={mission} className="mission" alt="" /> */}
+            <img src={construction} className="construction" alt="" />
+            <img src={mission} className="mission" alt="" />
         </div>
         
     ) 
