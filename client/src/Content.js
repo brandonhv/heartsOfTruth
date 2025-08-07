13eea659d25f8a6011ad2fcd33828330fe731efa
Brandon <brandon5.9@hotmail.com>
2025-08-07 14:09:41 -0400

    Change main fonts of the desktop and tablets for titles and quotes

--- client/src/Content.js
+++ client/src/Content.js
@@ -14,7 +14,7 @@ const Content = () => {
         return (
             <div className="content-container">
 
-                <h2 className="first-title">What are we currently working on?</h2>
+                <h2 className="content-title">What are we currently working on?</h2>
                 <blockquote> " Launching our first home in Cagayan de Oro City, Philippines... "</blockquote>
                 <div className="image-section">
                 <img src={mainImage} className="content-image" alt="Main House" />
@@ -26,7 +26,7 @@ const Content = () => {
 
                 
 
-                <h2>Community</h2>
+                <h2 className="content-title">Community</h2>
                 <blockquote>" Building homes for the people in the Philippines... "</blockquote>
                 <div className="image-section">
                 <img src={community} className="content-image" alt="Main House" />
@@ -37,7 +37,7 @@ const Content = () => {
                 <hr className="section-divider" />
 
 
-                <h2>Construction</h2>
+                <h2 className="content-title">Construction</h2>
                 <blockquote> " Creating homes from the ground up... " </blockquote>
                 <div className="image-section">
                 <img src={construction} className="content-image" alt="Main House" />
@@ -47,7 +47,7 @@ const Content = () => {
                 </div>
                 <hr className="section-divider" />
 
-                <h2>Mission</h2>
+                <h2 className="content-title">Mission</h2>
                 <blockquote>" Making an impact by changing people's trajectory of life, one home at a time... "</blockquote>
                 <div className="image-section">
                 <img src={mission} className="content-image" alt="Main House" />
